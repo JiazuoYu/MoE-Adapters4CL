f58ab63319948ecb109efeaedac95acf3167261a
JiazuoYu <149066808+JiazuoYu@users.noreply.github.com>
2024-03-16 19:37:07 +0800

    Add files via upload

--- mtil/scripts/test/Few_Shot_order1_test.sh
+++ mtil/scripts/test/Few_Shot_order1_test.sh
@@ -6,9 +6,8 @@ set -x
 
 GPU=3,4
 dataset=(Aircraft Caltech101 CIFAR100 DTD EuroSAT Flowers Food MNIST OxfordPet StanfordCars SUN397)
-dataset_test=(Aircraft Aircraft Caltech101 CIFAR100 DTD EuroSAT Flowers Food MNIST OxfordPet StanfordCars SUN397)
 lr=(5e-3 1e-4 5e-3 1e-3 1e-4 1e-3 1e-3 1e-4 1e-3 1e-3 1e-3)
-chooser=(TinyImagenet_autochooser Aircraft_autochooser Caltech101_autochooser CIFAR100_autochooser DTD_autochooser EuroSAT_autochooser Flowers_autochooser Food_autochooser MNIST_autochooser OxfordPet_autochooser StanfordCars_autochooser SUN397_autochooser)
+chooser=(Aircraft_autochooser Caltech101_autochooser CIFAR100_autochooser DTD_autochooser EuroSAT_autochooser Flowers_autochooser Food_autochooser MNIST_autochooser OxfordPet_autochooser StanfordCars_autochooser SUN397_autochooser)
 threshold=(600e-4 600e-4 600e-4 600e-4 600e-4 600e-4 600e-4 600e-4 600e-4 600e-4 600e-4 600e-4)
 num=22 # experts num
 
@@ -16,14 +15,14 @@ num=22 # experts num
 model_ckpt_path=ckpt/few_shot_order1_1000iters
 
 # inference
-for ((j = 1; j < 12; j++)); do
-  for ((i = 1; i < ${#dataset_test[@]}; i++)); do
-    dataset_cur=${dataset_test[j]}
+for ((j = 0; j < 11; j++)); do
+  for ((i = 0; i < ${#dataset[@]}; i++)); do
+    dataset_cur=${dataset[j]}
 
     CUDA_VISIBLE_DEVICES=${GPU} python -m src.main --eval-only \
         --train-mode=adapter \
         --eval-datasets=${dataset_cur} \
-        --load ${model_ckpt_path}/${dataset_test[i]}.pth \
+        --load ${model_ckpt_path}/${dataset[i]}.pth \
         --load_autochooser ${model_ckpt_path}/${chooser[i]}.pth \
         --data-location /home/dhw/yjz_workspace/data/data \
         --ffn_adapt_where AdapterDoubleEncoder \
--- mtil/scripts/test/Full_Shot_order1.sh
+++ mtil/scripts/test/Full_Shot_order1.sh
@@ -3,11 +3,10 @@
 set -v
 set -e
 set -x
-GPU=2,3
+GPU=3,4
 dataset=(Aircraft Caltech101 CIFAR100 DTD EuroSAT Flowers Food MNIST OxfordPet StanfordCars SUN397)
-dataset_test=(Aircraft Aircraft Caltech101 CIFAR100 DTD EuroSAT Flowers Food MNIST OxfordPet StanfordCars SUN397)
 lr=(5e-3 1e-3 5e-3 1e-3 1e-4 1e-3 1e-3 1e-4 1e-3 1e-3 1e-3)
-chooser=(TinyImagenet_autochooser Aircraft_autochooser Caltech101_autochooser CIFAR100_autochooser DTD_autochooser EuroSAT_autochooser Flowers_autochooser Food_autochooser MNIST_autochooser OxfordPet_autochooser StanfordCars_autochooser SUN397_autochooser)
+chooser=(Aircraft_autochooser Caltech101_autochooser CIFAR100_autochooser DTD_autochooser EuroSAT_autochooser Flowers_autochooser Food_autochooser MNIST_autochooser OxfordPet_autochooser StanfordCars_autochooser SUN397_autochooser)
 threshold=(655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4)
 num=22 # experts num
 
@@ -15,14 +14,14 @@ num=22 # experts num
 model_ckpt_path=ckpt/full_shot_order1_1000iters
 
 # inference
-for ((j = 1; j < 12; j++)); do
-  for ((i = 1; i < ${#dataset_test[@]}; i++)); do
-    dataset_cur=${dataset_test[j]}
+for ((j = 0; j < 11; j++)); do
+  for ((i = 0; i < ${#dataset[@]}; i++)); do
+    dataset_cur=${dataset[j]}
 
     CUDA_VISIBLE_DEVICES=${GPU} python -m src.main --eval-only \
         --train-mode=adapter \
         --eval-datasets=${dataset_cur} \
-        --load ${model_ckpt_path}/${dataset_test[i]}.pth \
+        --load ${model_ckpt_path}/${dataset[i]}.pth \
         --load_autochooser ${model_ckpt_path}/${chooser[i]}.pth \
         --data-location /home/dhw/yjz_workspace/data/data \
         --ffn_adapt_where AdapterDoubleEncoder \
